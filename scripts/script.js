b68f9e45c29355f7ff018aaa863334c9ddd52ef8
Sethumathavan G <sethupj@gmail.com>
2023-03-16 13:24:04 +0530

    add commit

--- scripts/script.js
+++ scripts/script.js
@@ -68,3 +68,21 @@ const typed = new Typed(".multiple-text", {
   backDelay: 1000,
   loop: true,
 });
+
+function sendMail() {
+  var params = {
+    name: document.getElementById("name").value,
+    email: document.getElementById("email").value,
+    message: document.getElementById("message").value,
+  };
+  const serviceID = "service_zd2xchm";
+  const templateID = "template_wcm2t9l";
+
+  emailjs.send(serviceID, templateID, params).then((res) => {
+    document.getElementById("name").value = "";
+    document.getElementById("email").value = "";
+    document.getElementById("message").value = "";
+    console.log(res);
+    alert("your message send successfully").catch((err) => console.log(err));
+  });
+}
